a58f3c5e1fa36d28d219a5a02a0fd19e88d9e02d
“liqiangisme” <liqiangit@outlook.com>
2024-05-04 09:53:10 +0800

    测试2

--- manage.py
+++ manage.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment hahaha?"
+            "forget to activate a virtual environment hahaha hahaha?"
         ) from exc
     execute_from_command_line(sys.argv)
